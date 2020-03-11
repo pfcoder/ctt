0ebf88d641d48dd641c23d6105536a2d4db6dd23
NikVolf <nikvolf@gmail.com>
2020-03-11 17:42:40 +0300

    make sure return ready iterator once state is updated

--- primitives/transaction-pool/src/pool.rs
+++ primitives/transaction-pool/src/pool.rs
@@ -29,7 +29,7 @@ use futures::{
 use serde::{Deserialize, Serialize};
 use sp_runtime::{
 	generic::BlockId,
-	traits::{Block as BlockT, Member},
+	traits::{Block as BlockT, Member, NumberFor},
 	transaction_validity::{
 		TransactionLongevity, TransactionPriority, TransactionTag,
 	},
@@ -211,7 +211,7 @@ pub trait TransactionPool: Send + Sync {
 
 	// *** Block production / Networking
 	/// Get an iterator for ready transactions ordered by priority
-	fn ready(&self) -> Box<dyn Iterator<Item=Arc<Self::InPoolTransaction>>>;
+	fn ready(&self, at: Option<NumberFor<Self::Block>>) -> Pin<Box<dyn Future<Output=Box<dyn Iterator<Item=Arc<Self::InPoolTransaction>> + Send>> + Send>>;
 
 	// *** Block production
 	/// Remove transactions identified by given hashes (and dependent transactions) from the pool.
