83a6eedac947a27520382781433968cc95e8ff0c
pfcoder <pfcoder97@gmail.com>
2020-04-10 17:18:56 +0800

    fix upgrade build errors

--- bin/ctt/node/src/main.rs
+++ bin/ctt/node/src/main.rs
@@ -8,16 +8,6 @@ mod cli;
 mod command;
 
 fn main() -> sc_cli::Result<()> {
-	let version = sc_cli::VersionInfo {
-		name: "CTT Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "ctt",
-		author: "ctt team",
-		description: "CTT Node",
-		support_url: "support.anonymous.an",
-		copyright_start_year: 2019,
-	};
-
-	command::run(version)
+	command::run()
 }
+
